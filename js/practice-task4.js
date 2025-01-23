9c7e1f737bf3c368c59e12df223b489112c53fd5
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-01-23 22:51:59 +0200

    Add exercises

--- js/practice-task4.js
+++ js/practice-task4.js
@@ -226,3 +226,24 @@ function getProductPrice(productName) {
 
 console.log(getProductPrice("Scanner")); // 2700
 console.log(getProductPrice("Engine")); // null
+
+function getAllPropValues(propName) {
+  const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+  ];
+
+  let propValues = [];
+
+  for (const product of products) {
+    if (product[propName]) {
+      propValues.push(product[propName]);
+    }
+  }
+  return propValues;
+}
+
+console.log(getAllPropValues("name"));
+console.log(getAllPropValues("category"));
