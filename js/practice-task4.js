d88ce353e38a87a62b784128f85cdf6760a15a5a
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-01-24 23:15:06 +0200

    Add exercises

--- js/practice-task4.js
+++ js/practice-task4.js
@@ -247,3 +247,25 @@ function getAllPropValues(propName) {
 
 console.log(getAllPropValues("name"));
 console.log(getAllPropValues("category"));
+
+function calculateTotalPrice(productName) {
+  const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+  ];
+
+  let totalPrice = 0;
+
+  for (const product of products) {
+    if (product.name === productName) {
+      totalPrice = product.price * product.quantity;
+    }
+  }
+  return totalPrice || `Product ${productName} not found!`;
+}
+
+console.log(calculateTotalPrice("Radar")); // 5200
+console.log(calculateTotalPrice("Blaster")); // "Product Blaster not found!"
+console.log(calculateTotalPrice("Grip")); // 10800
