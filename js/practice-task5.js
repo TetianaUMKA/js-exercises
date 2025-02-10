89ed4ce18ee9fc861af556a7002df12dc2867d61
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-02-10 21:36:39 +0200

    Add exercises

--- js/practice-task5.js
+++ js/practice-task5.js
@@ -857,3 +857,74 @@ const sortedByDescendingRating1 = books5.toSorted(
   (firstBook, secondBook) => secondBook.rating - firstBook.rating
 );
 console.log(sortedByDescendingRating1);
+
+// chaining
+
+const students7 = [
+  { name: "Mango", score: 83, courses: ["mathematics", "physics"] },
+  { name: "Poly", score: 59, courses: ["science", "mathematics"] },
+  { name: "Ajax", score: 37, courses: ["physics", "biology"] },
+  { name: "Kiwi", score: 94, courses: ["literature", "science"] },
+];
+
+const names1 = students7
+  .toSorted((a, b) => a.score - b.score)
+  .map((student) => student.name);
+
+console.log(names1); // ["Ajax", "Poly", "Mango", "Kiwi"]
+
+const uniqueSortedCourses = students7
+  .flatMap((student) => student.courses)
+  .filter((course, index, array) => array.indexOf(course) === index)
+  .toSorted((a, b) => a.localeCompare(b));
+
+console.log(uniqueSortedCourses); // ["biology", "science", "literature", "mathematics", "physics"]
+
+const allCourses = students7.flatMap((student) => student.courses);
+console.log(allCourses);
+const uniqueFilteredCourses = allCourses.filter(
+  (course, index, array) => array.indexOf(course) === index
+);
+console.log(uniqueFilteredCourses);
+
+let uniqueFilteredCourses2 = [];
+
+for (let i = 0; i < allCourses.length; i += 1) {
+  if (allCourses.indexOf(allCourses[i]) === i) {
+    uniqueFilteredCourses2.push(allCourses[i]);
+    console.log(allCourses.indexOf(allCourses[i]));
+    console.log(i);
+  }
+}
+console.log(uniqueFilteredCourses2);
+
+// task
+const books6 = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  {
+    title: "The Dreams in the Witch House",
+    author: "Howard Lovecraft",
+    rating: 8.67,
+  },
+];
+const MIN_BOOK_RATING = 8;
+
+const names2 = books6
+  .filter((book) => book.rating > MIN_BOOK_RATING)
+  .map((book) => book.author)
+  .toSorted((a, b) => a.localeCompare(b));
