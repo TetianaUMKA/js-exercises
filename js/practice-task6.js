d0f4aadde44dad64624b101fececd403e91c336d
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-02-17 23:32:04 +0200

    Add exercises

---
+++ js/practice-task6.js
@@ -0,0 +1,19 @@
+const pizzaPalace = {
+  pizzas: ["Supercheese", "Smoked", "Four meats"],
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
+
+    return `Order accepted, preparing «${pizzaName}» pizza`;
+  },
+};
+
+console.log(pizzaPalace.order("Smoked")); // "Order accepted, preparing «Smoked» pizza"
+console.log(pizzaPalace.order("Four meats")); // "Order accepted, preparing «Four meats» pizza"
+console.log(pizzaPalace.order("Big Mike")); // "Sorry, there is no pizza named «Big Mike»"
