172fbb01bea24857da0b21cb22a388878dab5d68
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-02-23 22:34:51 +0200

    Add exercises

--- js/practice-task6.js
+++ js/practice-task6.js
@@ -17,3 +17,13 @@ const pizzaPalace = {
 console.log(pizzaPalace.order("Smoked")); // "Order accepted, preparing «Smoked» pizza"
 console.log(pizzaPalace.order("Four meats")); // "Order accepted, preparing «Four meats» pizza"
 console.log(pizzaPalace.order("Big Mike")); // "Sorry, there is no pizza named «Big Mike»"
+
+// example
+const book = {
+  updateTitle(newTitle) {
+    console.log(newTitle, this);
+  },
+};
+const update = book.updateTitle;
+update("Lord Of The Rings"); // "Lord Of The Rings", undefined
+// In this case, we get such result as 'undefined' because turned on 'use strict' or type='module' of script // but we can get 'Window object' if turn off 'use strict'
