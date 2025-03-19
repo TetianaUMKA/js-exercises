55fb4a3d06f2d799ed65e11a2bcae4d34406a56e
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-03-19 23:57:46 +0200

    Add exercises

--- js/practice-task5.js
+++ js/practice-task5.js
@@ -1333,3 +1333,99 @@ const totalSalary = Object.values(allSalary).reduce((acc, salary) => {
   return (acc += salary);
 }, 0);
 console.log(totalSalary);
+
+const allCars2 = [
+  {
+    make: "Honda",
+    model: "CR-V",
+    type: "suv",
+    amount: 14,
+    price: 24045,
+    onSale: true,
+  },
+  {
+    make: "Honda",
+    model: "Accord",
+    type: "sedan",
+    amount: 2,
+    price: 22455,
+    onSale: true,
+  },
+  {
+    make: "Mazda",
+    model: "Mazda 6",
+    type: "sedan",
+    amount: 8,
+    price: 24195,
+    onSale: false,
+  },
+  {
+    make: "Mazda",
+    model: "CX-9",
+    type: "suv",
+    amount: 7,
+    price: 31520,
+    onSale: true,
+  },
+  {
+    make: "Toyota",
+    model: "4Runner",
+    type: "suv",
+    amount: 19,
+    price: 34210,
+    onSale: false,
+  },
+  {
+    make: "Toyota",
+    model: "Sequoia",
+    type: "suv",
+    amount: 16,
+    price: 45560,
+    onSale: false,
+  },
+  {
+    make: "Toyota",
+    model: "Tacoma",
+    type: "truck",
+    amount: 4,
+    price: 24320,
+    onSale: true,
+  },
+  {
+    make: "Ford",
+    model: "F-150",
+    type: "truck",
+    amount: 11,
+    price: 27110,
+    onSale: true,
+  },
+  {
+    make: "Ford",
+    model: "Fusion",
+    type: "sedan",
+    amount: 13,
+    price: 22120,
+    onSale: true,
+  },
+  {
+    make: "Ford",
+    model: "Explorer",
+    type: "suv",
+    amount: 6,
+    price: 31660,
+    onSale: false,
+  },
+];
+
+const getAvailableCarNames = (cars) => cars.filter((car) => car.onSale);
+console.log(getAvailableCarNames(allCars2));
+
+const getSortedCarsOnSale = (cars) =>
+  cars.filter((car) => car.onSale).toSorted((a, b) => a.price - b.price);
+console.log(getSortedCarsOnSale(allCars2));
+
+const getSortedCarsOnSaleByAlphabet = (cars) =>
+  cars
+    .filter((car) => car.onSale && car.price < 25000)
+    .toSorted((a, b) => a.model.localeCompare(b.model));
+console.log(getSortedCarsOnSaleByAlphabet(allCars2));
