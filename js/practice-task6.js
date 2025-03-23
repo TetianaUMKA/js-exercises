60dec70651e3cf85ee6b45836b65e4f381d34ac5
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-03-23 21:00:05 +0200

    Add exercises

--- js/practice-task6.js
+++ js/practice-task6.js
@@ -27,3 +27,75 @@ const book = {
 const update = book.updateTitle;
 update("Lord Of The Rings"); // "Lord Of The Rings", undefined
 // In this case, we get such result as 'undefined' because turned on 'use strict' or type='module' of script // but we can get 'Window object' if turn off 'use strict'
+
+// call
+function greet(str) {
+  console.log(`${str}, ${this.username}, your room is ${this.room}!`);
+}
+
+const user1 = {
+  username: "Maria",
+  room: 27,
+};
+
+const user2 = {
+  username: "Emma",
+  room: 191,
+};
+
+greet.call(user1, "Welcome");
+greet.call(user2, "Aloha");
+
+// just a test task is not used in real work
+function greet2(name) {
+  console.log(`Hello, ${name}! I am ${this.person}`);
+}
+const person = "John";
+const context = {
+  person: "Alice",
+};
+greet2.call(context, "Bob"); // "Hello, Bob! I am Alice"
+
+// apply
+function greet3(str) {
+  console.log(`${str}, ${this.username}, your room is ${this.room}!`);
+}
+
+const mango = {
+  username: "Mango",
+  room: 27,
+};
+
+const poly = {
+  username: "Poly",
+  room: 191,
+};
+
+greet3.apply(mango, ["Welcome"]); // "Welcome, Mango, your room is 27!"
+greet3.apply(poly, ["Aloha"]); // "Aloha, Poly, your room is 191!"
+
+// bind
+const customer = {
+  username: "Jacob",
+  sayHello() {
+    console.log(`Hello, ${this.username}!`);
+  },
+};
+
+customer.sayHello(); // "Hello, Jacob!"
+
+const greet = customer.sayHello.bind(customer);
+
+greet(); // "Hello, Jacob!"
+
+function sayHello(greeting) {
+  console.log(`${greeting}, ${this.name}!`);
+}
+
+const user = {
+  name: "Alice",
+};
+
+const greet = sayHello.bind(user);
+
+greet("Hello"); // "Hello, Alice"
