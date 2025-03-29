0a40d9ed3b7a7f4f7bae2b7487309da596c3f7bc
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-03-29 21:12:45 +0200

    Add exercises

--- js/practice-task6.js
+++ js/practice-task6.js
@@ -178,3 +178,28 @@ console.log(Object.values(dog2)); // ["Mango"]
 for (const key of Object.keys(dog2)) {
   console.log(key); // "name"
 }
+
+// Chaining of [[Prototype]]
+const objC = { c: "objC prop" };
+
+const objB = Object.create(objC);
+objB.b = "objB prop";
+
+const objA = Object.create(objB);
+objA.a = "objA prop";
+
+console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
+console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
+console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
+
+console.log(objA.hasOwnProperty("a")); // true
+console.log(objA.a); // "objA prop"
+
+console.log(objA.hasOwnProperty("b")); // false
+console.log(objA.b); // "objB prop"
+
+console.log(objA.hasOwnProperty("c")); // false
+console.log(objA.c); // "objC prop"
+
+console.log(objA.hasOwnProperty("x")); // false
+console.log(objA.x); // undefined
